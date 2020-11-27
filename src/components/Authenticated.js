cfcbb039dfc1a83baee1f0d202fff4289fcdae49
mirsahib <mirsahib24@gmail.com>
2020-11-27 09:22:59 +0600

    :truck:Move component file

---
+++ src/components/Authenticated.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Authenticated() {
+  return <div>This is Authenticated route</div>;
+}
+
+export default Authenticated;
---
+++ src/components/Unauthenticated.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Unauthenticated() {
+  return <div>This is Unauthenticated route</div>;
+}
+
+export default Unauthenticated;
