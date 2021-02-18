4ddcb96ac6f6624311ecc3bd1210804207634455
sArianV <yoarian94@gmail.com>
2021-02-17 23:06:02 -0300

    3r

--- src/helpers/db-helper.js
+++ src/helpers/db-helper.js
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 function createClient() {
-  let saslprep = require("saslprep");
   const client = new MongoClient(
     `mongodb+srv://inutiles2021:${process.env.REACT_APP_DB_PASSWORD}@cluster0.npkov.mongodb.net/test?retryWrites=true&w=majority`,
     {
